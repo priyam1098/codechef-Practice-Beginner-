cd435fddc4e5fec2e3db6551dafe0bb8435ef56d
Priyam <37201646+priyam1098@users.noreply.github.com>
2020-04-26 18:12:11 +0530

    Create FLOW008.C

---
+++ FLOW008.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int T,i,N;
+    scanf("%d",&T);
+    for(i=0;i<T;i++){
+        scanf("%d",&N);
+        if(-20<=N<=20){
+            if(N<10){
+               printf("What an obedient servant you are!\n");
+        }
+            else{
+               printf("%d\n",9-10);
+        }
+    }
+    }
+	return 0;
+}
+
